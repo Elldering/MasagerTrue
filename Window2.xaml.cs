f67f39a920be569b730adf34afaf325a533d8001
Eldering <102358566+Elldering@users.noreply.github.com>
2023-05-17 22:42:27 +0300

    2.0
    2.0

--- Window2.xaml.cs
+++ Window2.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
@@ -23,11 +24,14 @@ namespace Client
     {
         private Socket socket;
         private CancellationTokenSource isWorking;
+        string Name_podkl;
+        private string ClientId;
         public Window2(string user, string ip)
         {
             InitializeComponent();
             MessageBox.Show(user);
-
+            Name_podkl = user;
+            ClientId = Guid.NewGuid().ToString();
             try
             {
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -37,8 +41,10 @@ namespace Client
             catch (Exception)
             {
                 MessageBox.Show("Ошибка подключения");
-                return;
+                this.Close();
+
             }
+            send(" ");
         }
 
         private async Task RecieveMessage(string user)
@@ -47,16 +53,64 @@ namespace Client
             {
                 byte[] bytes = new byte[1024];
                 await socket.ReceiveAsync(bytes, SocketFlags.None);
-                string message = Encoding.UTF8.GetString(bytes);
 
-                AllMessage.Items.Add(DateTime.Now.ToString() + " [" + user + "]: " + message);
+                string json = Encoding.UTF8.GetString(bytes);
+                Dictionary<string, string> userNames = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                List<string> AlU = new List<string>();
+                foreach (var kvp in userNames)
+                {
+                    string userName = kvp.Key;
+                    string message_recieve = kvp.Value;
+
+                    if (message_recieve == "/AllUsers")
+                    {
+                        if (!AllUsers.Items.Contains(userName))
+                        {
+                            AlU.Add(userName);
+                        }
+                    }
+                    else
+                    {
+                        AllMessage.Items.Add($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {userName}: {message_recieve}");
+                    }
+
+                    if (message_recieve == "/disconnect")
+                    {
+                        MessageBox.Show("Сервер завершил сессию");
+                        this.Close();
+                    }
+                }
+
+                for (int i = AllUsers.Items.Count - 1; i >= 0; i--)
+                {
+                    string item = AllUsers.Items[i].ToString();
+                    if (!AlU.Contains(item))
+                    {
+                        AllUsers.Items.RemoveAt(i);
+                    }
+                }
+                AllUsers.Items.Clear();
+                foreach (string userName in AlU)
+                {
+                    AllUsers.Items.Add(userName);
+                }
+                AlU.Clear();
+
             }
         }
 
+
         private async Task send(string msg)
         {
-            byte[] bytes = Encoding.UTF8.GetBytes(msg);
+            Dictionary<string, string> messageData = new Dictionary<string, string>();
+            messageData.Add(Name_podkl, msg);
+
+            string json = JsonConvert.SerializeObject(messageData);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
             await socket.SendAsync(bytes, SocketFlags.None);
+            messageData.Clear();
+
+
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
@@ -66,7 +120,37 @@ namespace Client
 
         private void send_BTN_Click(object sender, RoutedEventArgs e)
         {
-            send(Massage_TextBox.Text);
+            if (Massage_TextBox.Text == "/disconnect")
+            {
+                MainWindow mainWindow = new MainWindow();
+                send(Massage_TextBox.Text);
+
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                send(Massage_TextBox.Text);
+            }
+        }
+
+        private void out_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MainWindow mainWindow = new MainWindow();
+            send("/disconnect");
+            this.Close();
+            return;
+        }
+
+        private void Client_window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            send("/disconnect");
+            MainWindow dd = new MainWindow();
+            dd.Show();
+            e.Cancel = false;
+
+
         }
     }
 }
